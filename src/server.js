e3b9d6eeb62f2ef6dfc7e63849b802bf77f1f4a0
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2016-10-25 10:51:33 +1100

    added some proper startup text

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
 import chalk from 'chalk'
+import fs from 'fs'
 // Import local libs
 import mkdir from './library/mkdir'
 import options from './library/options'
@@ -13,6 +14,7 @@ const dateTime = new Date().toISOString()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.post('/', (req, res) => {
   if (req.body.Action === 'SendEmail') {
     log(`
@@ -21,9 +23,10 @@ app.post('/', (req, res) => {
       ${chalk.blue('To:')} ${req.body['Destination.ToAddresses.member.1']},
       ${chalk.blue('Subject:')} ${req.body['Message.Subject.Data']},
     `)
-    mkdir(path.join(`${options.outputDir}/${dateTime.slice(0, 10)}/${dateTime.slice(11, 22)}`))
-    // Html: req.body['Message.Body.Html.Data']
-    // Text: req.body['Message.Body.Text.Data']
+    let dir = `${options.outputDir}/${dateTime.slice(0, 10)}/${dateTime.slice(11, 22)}`
+    mkdir(path.join(dir))
+    fs.writeFileSync(`${dir}/body.html`, req.body['Message.Body.Html.Data']);
+    fs.writeFileSync(`${dir}/body.txt`, req.body['Message.Body.Text.Data']);
   }
   res.status(200).send('Were up\n')
 })
@@ -31,5 +34,7 @@ app.post('/', (req, res) => {
 app.listen(options.port)
 
 log(`
+${chalk.inverse('  AWS Simple Email Service Local 📬  ')}
   ${chalk.green('Listening on port:')} ${options.port}
+  ${chalk.green('Output directory:')} ${options.outputDir}
 `)
