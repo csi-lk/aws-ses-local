1f380dd1bf04407426503c960d36bcb84bdc942a
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2016-10-25 11:26:11 +1100

    as it turns out my string replace wasn't working

--- src/server.js
+++ src/server.js
@@ -23,21 +23,22 @@ app.post('/', (req, res) => {
       ${chalk.blue('To:')} ${req.body['Destination.ToAddresses.member.1']}
       ${chalk.blue('Subject:')} ${req.body['Message.Subject.Data']}
     `)
-    const dir = `${options.outputDir}/${dateTime.slice(0, 10)}/${dateTime.slice(11, 22).replace(':', '.')}`
+    const dir = `${options.outputDir}/${dateTime.slice(0, 10)}/${dateTime.slice(11, 22).replace(/:\s*/g, '.')}`
+    console.log(`${dateTime.slice(11, 22)}`)
     const headers = `Subject: ${req.body['Message.Subject.Data']}\nDestination: ${req.body['Destination.ToAddresses.member.1']}\nSource: ${req.body.Source}`
     mkdir(path.join(dir))
     fs.writeFileSync(`${dir}/body.html`, req.body['Message.Body.Html.Data'])
     fs.writeFileSync(`${dir}/body.txt`, req.body['Message.Body.Text.Data'])
     fs.writeFileSync(`${dir}/headers.txt`, headers)
+    res.status(200).send(`
+      <?xml version="1.0" encoding="UTF-8"?>
+      <SendEmailResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+        <SendEmailResult>
+          <MessageId>${process.cwd() + path.join(dir) + '/body.html'}</MessageId>
+        </SendEmailResult>
+      </SendEmailResponse>
+    `)
   }
-  res.status(200).send(`
-    <?xml version="1.0" encoding="UTF-8"?>
-    <SendEmailResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
-      <SendEmailResult>
-        <MessageId>1</MessageId>
-      </SendEmailResult>
-    </SendEmailResponse>
-  `)
 })
 
 app.listen(options.port)
