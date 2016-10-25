551d9df2b7325c8bd9b92632ba8d75aa90a696b4
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2016-10-25 12:04:07 +1100

    added error response with template

--- src/server.js
+++ src/server.js
@@ -11,14 +11,14 @@ import rmdir from './library/rmdir'
 
 const app = express()
 const log = console.log
-const successTemplate = fs.readFileSync(`${__dirname}/templates/success.xml`, { encoding: 'utf-8' });
-const errorTemplate = fs.readFileSync(`${__dirname}/templates/error.xml`, { encoding: 'utf-8' });
+const successTemplate = fs.readFileSync(`${__dirname}/templates/success.xml`, { encoding: 'utf-8' })
+const errorTemplate = fs.readFileSync(`${__dirname}/templates/error.xml`, { encoding: 'utf-8' })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 log(`
-${chalk.inverse('  AWS Simple Email Service Local 📪  ')}
+${chalk.inverse('  AWS Simple Email Service Local 📪   ')}
   ${chalk.green('Listening on port:')} ${options.port}`)
 
 if (options.clean !== undefined) {
@@ -35,19 +35,28 @@ app.post('/', (req, res) => {
     const dateDir = `${options.outputDir}/${dateTime.slice(0, 10)}`
     const fullDir = `${dateDir}/${dateTime.slice(11, 22).replace(/:\s*/g, '.')}`
     const headers = `Subject: ${req.body['Message.Subject.Data']}\nDestination: ${req.body['Destination.ToAddresses.member.1']}\nSource: ${req.body.Source}`
-    mkdir(path.join(dateDir))
-    mkdir(path.join(fullDir))
-    log(`  📬  ${chalk.green('Email Recieved')}
-      ${chalk.blue('From:')} ${req.body.Source}
-      ${chalk.blue('To:')} ${req.body['Destination.ToAddresses.member.1']}
-      ${chalk.blue('Subject:')} ${req.body['Message.Subject.Data']}
-      ${chalk.blue('Html Email:')} ${process.cwd()}/${path.join(fullDir)}/body.html
-      ${chalk.blue('Text Email:')} ${process.cwd()}/${path.join(fullDir)}/body.txt
-    `)
-    fs.writeFileSync(`${fullDir}/body.html`, req.body['Message.Body.Html.Data'])
-    fs.writeFileSync(`${fullDir}/body.txt`, req.body['Message.Body.Text.Data'])
-    fs.writeFileSync(`${fullDir}/headers.txt`, headers)
-    res.status(200).send(successTemplate.replace('{{message}}', `${process.cwd()}/${path.join(fullDir)}/body.html`))
+    try {
+      mkdir(path.join(dateDir))
+      mkdir(path.join(fullDir))
+      log(`  📬  ${chalk.green('Email Recieved')}
+        ${chalk.blue('From:')} ${req.body.Source}
+        ${chalk.blue('To:')} ${req.body['Destination.ToAddresses.member.1']}
+        ${chalk.blue('Subject:')} ${req.body['Message.Subject.Data']}
+        ${chalk.blue('Html Email:')} ${process.cwd()}/${path.join(fullDir)}/body.html
+        ${chalk.blue('Text Email:')} ${process.cwd()}/${path.join(fullDir)}/body.txt
+      `)
+      fs.writeFileSync(`${fullDir}/body.html`, req.body['Message.Body.Html.Data'])
+      fs.writeFileSync(`${fullDir}/body.txt`, req.body['Message.Body.Text.Data'])
+      fs.writeFileSync(`${fullDir}/headers.txt`, headers)
+      res.status(200).send(
+        successTemplate.replace('{{message}}', `${process.cwd()}/${path.join(fullDir)}/body.html`)
+      )
+    } catch (err) {
+      log(`   ${chalk.red('Error Occured:')} ${err}`)
+      res.status(500).send(
+        errorTemplate.replace('{{code}}', 'MessageRejected').replace('{{message}}', err.message)
+      )
+    }
   }
 })
 
