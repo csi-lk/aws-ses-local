7fb053be28a72f3d2587a85e75603c36b9e09409
Callum Silcock <csilk@Callums-MacBook-Pro.local>
2016-10-25 13:50:22 +1100

    fixed linting, wrote first succeed test

--- src/server-spec.js
+++ src/server-spec.js
@@ -1,4 +1,4 @@
-import chai,{expect} from 'chai'
+import chai, { expect } from 'chai'
 import chaiHttp from 'chai-http'
 import parser from 'xml2json'
 import fs from 'fs'
@@ -15,18 +15,18 @@ describe('/POST email', () => {
   it('it should succeed if email has all params', (done) => {
     chai.request(server)
       .post('/')
-      .send({ 
+      .send({
         Action: 'SendEmail',
         'Destination.ToAddresses.member.1': toAddress,
         'Message.Body.Html.Data': htmlEmail,
         'Message.Body.Text.Data': textEmail,
         'Message.Subject.Data': emailSubject,
-        Source: fromEmail
+        Source: fromEmail,
       })
       .end((err, res) => {
-        const json = parser.toJson(res.text, {object: true})
+        const json = parser.toJson(res.text, { object: true })
         expect(res).to.have.status(200)
-        expect(json.SendEmailResponse.SendEmailResult).to.have.MessageId
+        expect(json.SendEmailResponse.SendEmailResult).should.have.property('MessageId')
         expect(fs.readFileSync(json.SendEmailResponse.SendEmailResult.MessageId, 'utf8')).to.eq(htmlEmail)
         done()
       })
